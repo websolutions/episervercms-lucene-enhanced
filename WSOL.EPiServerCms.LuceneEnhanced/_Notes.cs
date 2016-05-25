8b4be33e37a41b4b65743462e1c227dfb03533ef
Brad McDavid <brad@wsol.com>
2016-05-25 14:02:36 -0400

    added itextsharp example as well.

--- WSOL.EPiServerCms.LuceneEnhanced/_Notes.cs
+++ WSOL.EPiServerCms.LuceneEnhanced/_Notes.cs
@@ -8,5 +8,9 @@
         //TODONE: Make init for document indexing event        
         //TODONE: Make readme and release notes
         //TODONE: Make default search service
+        //TODONE: https://jstemerdink.wordpress.com/2014/04/05/indexing-blocks-with-episerver-search/
+        //TODONE: https://tedgustaf.com/blog/2013/add-custom-fields-to-the-episerver-search-index-with-episerver-7/
+        //TODONE: https://github.com/dejancaric/EPi.SimpleSearch // nice service with index scheduled job
+        //ISearchable; // interface applied to models to keep from searching if returning false.
     }
 }
